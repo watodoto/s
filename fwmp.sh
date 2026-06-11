f0f337d96cb08bf80f8f2131dd1a6352a5e780f7
wato <onionhop@gmail.com>
2026-06-10 21:08:23 -0400

    Update fwmp.sh

--- fwmp.sh
+++ fwmp.sh
@@ -2,11 +2,13 @@
 # now with 1 billion times more skid
 # get ready for a straight vertical line of echo 🔥☠️🐋
 echo
+
 echo "----- Take back ownership of TPM -----"
 echo "> Take back TPM in tpm_manager_client..."
 tpm_manager_client take_ownership >/dev/null
 echo "--------------------------------------"
-echo
+
+echo # skid newline
 
 echo "------------ Remove FWMP -------------"
 echo "> Remove FWMP in cryptohome..."
@@ -16,7 +18,8 @@ device_management_client --action=remove_firmware_management_parameters >/dev/nu
 echo "> Set FWMP flags to 0..."
 device_management_client --action=set_firmware_management_parameters --flags=0x0000 >/dev/null
 echo "--------------------------------------"
-echo
+
+echo # skid newline 2
 
 echo "----- Unblocking developer mode ------"
 echo "> Remove devmode block in VPD..."
@@ -24,7 +27,8 @@ vpd -i RW_VPD -s block_devmode=0 >/dev/null
 echo "> Remove devmode block in crossystem..."
 crossystem block_devmode=0 >/dev/null
 echo "--------------------------------------"
-echo
+
+echo # skid newline 3
 
 echo "Done!"
 echo "This is just modmium's fwmp.sh but I added more shit to it."
