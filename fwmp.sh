63fcafac440e59d0336a2c7ea514c70b835e23ee
wato <onionhop@gmail.com>
2026-06-10 21:06:04 -0400

    Update fwmp.sh

--- fwmp.sh
+++ fwmp.sh
@@ -1,34 +1,28 @@
 #!/bin/bash
 # now with 1 billion times more skid
-
-echo -e
-echo -e "----- Take back ownership of TPM -----"
-echo -e "> Take back TPM in tpm_manager_client..."
+# get ready for a straight vertical line of echo 🔥☠️🐋
+echo
+echo "----- Take back ownership of TPM -----"
+echo "> Take back TPM in tpm_manager_client..."
 tpm_manager_client take_ownership >/dev/null
-echo -e "--------------------------------------"
-echo -e
-sleep 1
-
-echo -e "------------ Remove FWMP -------------"
-echo -e "> Remove FWMP in cryptohome..."
+echo "--------------------------------------"
+echo
+echo "------------ Remove FWMP -------------"
+echo "> Remove FWMP in cryptohome..."
 cryptohome --action=set_firmware_management_parameters --flags=0 >/dev/null
-echo -e "> Remove FWMP in device_management_client..."
+echo "> Remove FWMP in device_management_client..."
 device_management_client --action=remove_firmware_management_parameters >/dev/null
-echo -e "> Set FWMP flags to 0..."
+echo "> Set FWMP flags to 0..."
 device_management_client --action=set_firmware_management_parameters --flags=0x0000 >/dev/null
-echo -e "--------------------------------------"
-echo -e
-sleep 1
-
-echo -e "----- Unblocking developer mode ------"
-echo -e "> Remove devmode block in VPD..."
+echo "--------------------------------------"
+echo
+echo "----- Unblocking developer mode ------"
+echo "> Remove devmode block in VPD..."
 vpd -i RW_VPD -s block_devmode=0 >/dev/null
-echo -e "> Remove devmode block in crossystem..."
+echo "> Remove devmode block in crossystem..."
 crossystem block_devmode=0 >/dev/null
-echo -e "--------------------------------------"
-echo -e
-sleep 1
-
-echo -e "Done!"
-echo -e "This is just modmium's fwmp.sh but I added more shit to it."
-echo -e "- wato"
+echo "--------------------------------------"
+echo
+echo "Done!"
+echo "This is just modmium's fwmp.sh but I added more shit to it."
+echo "- wato"
