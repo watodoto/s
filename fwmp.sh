fa30a6d07240edd1382f3fb58f1da828d99539ca
wato <onionhop@gmail.com>
2026-06-10 20:02:37 -0400

    skkid
    dksdsdidskiddd

--- fwmp.sh
+++ fwmp.sh
@@ -4,6 +4,7 @@ echo -e "Take back ownership of TPM..."
 echo -e "Take back TPM in tpm_manager_client..."
 tpm_manager_client take_ownership
 sleep 1
+echo -e
 
 echo -e "Remove Firmware Management Parameters..."
 echo -e "> Remove FWMP in cryptohome..."
@@ -13,6 +14,7 @@ device_management_client --action=remove_firmware_management_parameters
 echo -e "> Set FWMP flags to 0..."
 device_management_client --action=set_firmware_management_parameters --flags=0x0000
 sleep 1
+echo -e
 
 echo -e "Unblocking developer mode..."
 echo -e "> Remove devmode block in VPD..."
@@ -20,6 +22,7 @@ vpd -i RW_VPD -s block_devmode=0
 echo -e "> Remove devmode block in crossystem..."
 crossystem block_devmode=0
 sleep 1
+echo -e
 
 echo -e "Done!"
 sleep 1
